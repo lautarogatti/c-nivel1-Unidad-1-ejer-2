64cb04819af536d17e32128d3fe0fbd42838955d
lautaro gatti <lautarogatti98@gmail.com>
2024-04-25 01:11:32 -0300

    subida a repo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace elevadorAlCubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.WriteLine("Bienvenido a el elevador al cubo, por favor inserte el numero a ser calculado:");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = n1 * n1 * n1;
+            Console.WriteLine("El resultado de elevar al cubo " + n1 + " es " + n2);
+            Console.WriteLine("Gracias por usar el elevador al cubo");
+        }
+    }
+}
